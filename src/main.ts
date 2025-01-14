8d63dc5e9e7bfcdf77dfc3292e3efe04bade9e95
ratchapolttum <ratchapolttum@gmail.com>
2025-01-14 09:44:55 +0700

    feat: init the application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express";
+
+import express from "express";
+
+import { AppModule } from "./app/app.module";
+
 class Application {
+  private static readonly logger: Logger = new Logger("Bootstrap");
+
   public static async run(): Promise<void> {
-    console.log("Application service apis started successfully");
+    try {
+      const application: NestExpressApplication = await NestFactory.create(AppModule, new ExpressAdapter(express()));
+
+      await application.init();
+      await application.listen(8080, "0.0.0.0");
+
+      this.logger.log("Running in development mode");
+      this.logger.log(`Listening on ${await application.getUrl()}`);
+      this.logger.log("Application started successfully");
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        this.logger.error(error.message, error.stack);
+      }
+    }
   }
 }
 
