5b8335c2229cabc70ced70bb5d325bf18c200568
ratchapolttum <ratchapolttum@gmail.com>
2025-01-14 07:15:28 +0700

    build: set up Webpack configuration for compile the source code

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const path = require("node:path");
+const sourceMapSupport = require("source-map-support");
+
+sourceMapSupport.install({
+  environment: "node",
+  handleUncaughtExceptions: true,
+  hookRequire: true
+});
+
+/**
+ * Compile the source code
+ *
+ * @param {import("webpack").Configuration} initial
+ *
+ * @returns {import("webpack").Configuration}
+ */
+module.exports = function compile(initial) {
+  return {
+    ...initial,
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "commonjs2"
+    },
+    target: "node",
+    mode: ["staging", "production"].includes(process.env.NODE_ENV) ? "production" : "development",
+    devtool: "source-map",
+    optimization: {
+      ...initial.optimization,
+      minimizer: [
+        new TerserWebpackPlugin({
+          extractComments: false,
+          parallel: true,
+          terserOptions: {
+            ecma: 2020,
+            keep_classnames: true,
+            keep_fnames: true,
+            mangle: true
+          }
+        })
+      ]
+    },
+    plugins: [
+      ...initial.plugins,
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "resources",
+            to: "resources"
+          }
+        ]
+      })
+    ]
+  };
+};
