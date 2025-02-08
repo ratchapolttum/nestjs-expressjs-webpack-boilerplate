a45811914a639189b29aac30ba344ef40ea46b29
ratchapolttum <ratchapolttum@gmail.com>
2025-02-08 23:28:34 +0700

    chore(lint): set up Prettier to enforce code formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const configuration = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  printWidth: 120,
+  quoteProps: "consistent",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
+
+export default configuration;
