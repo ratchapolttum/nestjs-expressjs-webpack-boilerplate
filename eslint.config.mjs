48e7684349e75087bfd1f68c23d7c747c32812f3
ratchapolttum <ratchapolttum@gmail.com>
2025-02-09 21:48:08 +0700

    chore(eslint): add TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -135,6 +135,10 @@ const configuration = typescriptEslint.config(
       sourceType: "module"
     },
     rules: {
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unused-vars": "off"
     }
   },
